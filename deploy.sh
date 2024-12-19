f25a73dbb83b7572fb6f6167a3cbc7522c94afe4
shokhzodmurodov <shaxa882@gmail.com>
2024-12-19 14:34:01 +0500

    added from secrets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 cd ~/Events/EventsBookingTest
 export PAYME_LOGIN=$1
 export PAYME_PASSWORD=$2
+export TELEGRAM_WEBHOOK=$3
+export TELEGRAM_API=$4
 git pull origin main
 docker compose build
 docker compose up -d
